b8292013616d084b8fc4d25f6cc02d5a81721d74
Hailey Au Yeung <hailey.auyeung@education.nsw.gov.au>
2023-05-04 10:43:13 +1000

    added form load and writelin

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hi");
+        }
     }
 }
